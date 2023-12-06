0f31df2145b364cfff525276a542b9d4d1f69f82
Travis Hawk <hawk.tr@gmail.com>
2023-12-05 20:02:04 -0800

    Got workable data, but still working on solution.

--- Day2/script.js
+++ Day2/script.js
@@ -9,19 +9,66 @@ fs.readFile("./Day2/data2.txt", 'utf8', (err, data) => {
   var redMax = 12;
   var greenMax = 13;
   var blueMax = 14;
+  var cleanPulls = [];
+  var numBlue = 0;
+  var numGreen = 0;
+  var numRed = 0;
+  var gameID = 0;
 
   const lines = data.split('\n');
   lines.forEach((line) => {
     
+      //Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 
-    handsSplit = line.split(';');
+
+    handsSplit = line.split(/:|;/);
     
-    console.log(lineParts);
-    //Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-    let numBlue = 0;
-    let numGreen = 0;
-    let numRed = 0;
-    let gameID = 0;
+    for(var i = 0; i<=handsSplit.length-1;i++){
+      if(i==0){
+        console.log(`${handsSplit[0]}`);
+      }
+      else{
+        pulls = handsSplit[i].split(' ');
+        cleanPulls = pulls.map(item => item.trim().replace(/,/g, '')).filter(Boolean);
+        
+        console.log(cleanPulls);
+
+      }
+
+      
+      if(i==1){
+        if(cleanPulls[i]=='blue'){
+          numBlue = cleanPulls[i-1];
+        }else if(cleanPulls[i]=='green'){
+          numBlue = cleanPulls[i-1];
+        }else if(cleanPulls[i]=='red'){
+          numRed=cleanPulls[i-1];
+        }
+      }
+      if(i==3){
+        if(cleanPulls[i]=='blue'){
+          numBlue = cleanPulls[i-1];
+        }else if(cleanPulls[i]=='green'){
+          numBlue = cleanPulls[i-1];
+        }else if(cleanPulls[i]=='red'){
+          numRed=cleanPulls[i-1];
+        }
+      }
+      if(i==5){
+        if(cleanPulls[i]=='blue'){
+          numBlue = cleanPulls[i-1];
+        }else if(cleanPulls[i]=='green'){
+          numBlue = cleanPulls[i-1];
+        
+        }else if(cleanPulls[i]=='red'){
+          numRed=cleanPulls[i-1];
+        
+        }
+      }
+      
+      console.log(`Reds: ${numRed}. Green: ${numGreen}. Blue: ${numBlue}.`)
+    }
+
 
  
 
